f2057f7a46f299c5a2fe92676192d9400354e0bc
Sung <sung272012@hotmail.com>
2024-09-21 03:03:18 -0400

    Add state to update person's information. For now, only name, phone number, email address, and address are stored in a state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import Form from './components/Form';
+import Resume from './components/Resume';
 
 function App() {
+  const [person, setPerson] = useState({
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    emailAddress: '',
+    address: '',
+  });
+
+  function handleFirstNameChange(e) {
+    setPerson({ ...person, firstName: e.target.value });
+  }
+  function handleLastNameChange(e) {
+    setPerson({ ...person, lastName: e.target.value });
+  }
+  function handlePhoneChange(e) {
+    setPerson({ ...person, phoneNumber: e.target.value });
+  }
+  function handleEmailChange(e) {
+    setPerson({ ...person, emailAddress: e.target.value });
+  }
+  function handleAddressChange(e) {
+    setPerson({ ...person, address: e.target.value });
+  }
+
   return (
     <>
       <h1>CV application</h1>
+      <Form
+        person={person}
+        onFirst={handleFirstNameChange}
+        onLast={handleLastNameChange}
+        onPhone={handlePhoneChange}
+        onEmail={handleEmailChange}
+        onAddress={handleAddressChange}
+      />
+      <Resume person={person} />
     </>
   );
 }
